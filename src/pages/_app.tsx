97cdf8682067c5a9d49e5d46dfae768452ff897b
Arthur DELORT <arthurdelort@hotmail.fr>
2021-08-25 10:19:59 +0200

    Create the Apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
+import { ClientOnly } from '../../graphql/components/ClientOnly'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache(),
+})
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ClientOnly>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ClientOnly>
+  )
 }
 
 export default App
