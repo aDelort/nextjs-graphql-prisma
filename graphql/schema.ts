9eaa1b0cce97f058190145bccae6d2ff68089fd4
Arthur DELORT <arthurdelort@hotmail.fr>
2021-08-25 11:31:12 +0200

    Create an Apollo server with Nexus and Prisma

---
+++ graphql/schema.ts
@@ -0,0 +1,19 @@
+import { makeSchema } from 'nexus'
+import { nexusPrisma } from 'nexus-plugin-prisma'
+import { join } from 'path'
+import { db } from './db'
+// import * as types from './resolvers'
+
+export const schema = makeSchema({
+  types: {},
+  outputs: {
+    typegen: join(__dirname, '../generated/nexus-typegen.ts'),
+    schema: join(__dirname, '../generated/schema.graphql'),
+  },
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+      prismaClient: (ctx) => (ctx.prisma = db),
+    }),
+  ],
+})
