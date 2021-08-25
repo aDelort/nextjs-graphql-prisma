eaa5067a85daed020578b84c97c87963e302b0e3
Arthur DELORT <arthurdelort@hotmail.fr>
2021-08-25 10:05:48 +0200

    Create the GraphQL resolvers according to the database schema

---
+++ graphql/resolvers/Post.ts
@@ -0,0 +1,33 @@
+import { extendType, objectType } from 'nexus';
+
+export const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id();
+    t.model.title();
+    t.model.body();
+    t.model.published();
+    t.model.authorId();
+    t.model.author();
+  },
+});
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.post();
+    t.crud.posts({ filtering: true });
+  },
+});
+
+export const PostMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.crud.createOnePost();
+    t.crud.deleteManyPost();
+    t.crud.deleteOnePost();
+    t.crud.updateManyPost();
+    t.crud.updateOnePost();
+    t.crud.upsertOnePost();
+  },
+});
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -2,10 +2,10 @@ import { makeSchema } from 'nexus'
 import { nexusPrisma } from 'nexus-plugin-prisma'
 import { join } from 'path'
 import { db } from './db'
-// import * as types from './resolvers'
+import * as types from './resolvers'
 
 export const schema = makeSchema({
-  types: {},
+  types,
   outputs: {
     typegen: join(__dirname, '../generated/nexus-typegen.ts'),
     schema: join(__dirname, '../generated/schema.graphql'),
